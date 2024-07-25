3f766e6d6858bcce1b3cdc5af2e5ea0d70a0720b
N8MARERahul <rahulpal521843@gmail.com>
2024-07-25 21:07:00 +0530

    Testing

--- Assignment-2/Java2Python/Q_1.py
+++
@@ -1,8 +0,0 @@
-# Write a Java program to check whether a number is Buzz or not.
-
-def isBuzz(num):
-    return num % 7 == 0 or num % 10 == 7
-
-num = int(input("Enter a number: "))
-
-print(f"{num} is {'' if isBuzz(num) else 'not '}a Buzz Number")
